70aefe87cae09a2a155140bbc6d71a01ed82871a
Miguel Montiel <31743982+miguelmontiel30@users.noreply.github.com>
2025-02-23 09:57:20 -0600

    [feat] Se agregaron nuevas dependencias y configuraciones. - Se agrego react-dropzone, swiper, tailwind-merge, @svgr/webpack, autoprefixer, prettier, prettier-plugin-tailwindcss.

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/** prettier.config.js */
+module.exports = {
+    plugins: ['prettier-plugin-tailwindcss'],
+
+    printWidth: 100,
+    tabWidth: 4,
+    singleQuote: true,
+    jsxSingleQuote: false,
+    trailingComma: 'all',
+    bracketSpacing: false,
+    bracketSameLine: false,
+    arrowParens: 'avoid',
+    endOfLine: 'auto',
+};
