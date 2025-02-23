7e1826ca76c9b62a7ff9e975eb9b6ac96e0a3c33
Miguel Montiel <31743982+miguelmontiel30@users.noreply.github.com>
2025-02-23 10:19:19 -0600

    [feat] Se agregaron nuevos componentes de formulario para trabajar sobre la app. - Se agregaron componentes comunes e inputs para formularios.

---
+++ src/components/form/input/Checkbox.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+interface CheckboxProps {
+    label?: string; // Optional label for the checkbox
+    checked: boolean; // Checked state
+    className?: string;
+    id?: string; // Unique ID for the checkbox
+    onChange: (checked: boolean) => void; // Change handler
+    disabled?: boolean; // Disabled state
+}
+
+const Checkbox: React.FC<CheckboxProps> = ({
+    label,
+    checked,
+    id,
+    onChange,
+    className = '',
+    disabled = false,
+}) => {
+    return (
+        <label
+            className={`flex cursor-pointer items-center space-x-3 ${
+                disabled ? 'cursor-not-allowed opacity-60' : ''
+            }`}
+        >
+            <input
+                id={id}
+                type="checkbox"
+                className={`h-4 w-4 ${className} dark:focus:bg-outline-none rounded border-gray-300 text-brand-500 focus:outline-none focus:ring-0 focus:ring-transparent focus:ring-offset-0 dark:border-gray-700 dark:bg-gray-800 dark:focus:outline-none dark:focus:ring-0 dark:focus:ring-transparent`}
+                checked={checked}
+                onChange={e => onChange(e.target.checked)}
+                disabled={disabled}
+            />
+            {label && (
+                <span className="text-theme-sm font-medium text-gray-800 dark:text-white">
+                    {label}
+                </span>
+            )}
+        </label>
+    );
+};
+
+export default Checkbox;
