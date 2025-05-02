626e44581075fe733ad98b4b4201f3f91c32be21
Miguel Montiel <31743982+miguelmontiel30@users.noreply.github.com>
2025-05-01 21:26:41 -0600

    [refactor] Reorganización de inserciones de datos de escuelas en scripts SQL. - Se eliminaron las inserciones de datos de escuelas del archivo `EduSync.data.sql` y se trasladaron a un nuevo archivo `School.data.sql`, mejorando la estructura y organización de los scripts SQL. - Esta modificación facilita el mantenimiento y la gestión de datos relacionados con las escuelas, evitando duplicaciones innecesarias y optimizando la carga de datos en el sistema educativo.

---
+++ src/DB/Data/School.data.sql
@@ -0,0 +1,42 @@
+INSERT INTO
+    schools (
+        name,
+        code,
+        address,
+        phone,
+        email,
+        website,
+        logo_url,
+        delete_flag
+    )
+VALUES
+    (
+        'Escuela 1',
+        'ESC123',
+        'Calle Principal 123',
+        '1234567890',
+        'escuela1@example.com',
+        'https://www.escuela1.com',
+        'logo1.png',
+        FALSE
+    ),
+    (
+        'Escuela 2',
+        'ESC456',
+        'Avenida Central 456',
+        '5551112222',
+        'escuela2@example.com',
+        'https://www.escuela2.com',
+        'logo2.png',
+        FALSE
+    ),
+    (
+        'Escuela 3',
+        'ESC789',
+        'Boulevard Independencia 789',
+        '5553334444',
+        'escuela3@example.com',
+        'https://www.escuela3.com',
+        'logo3.png',
+        FALSE
+    );
