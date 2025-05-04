6c4bfde26f6a3bbc03a7f2ecdfb80994cd80f9e0
Miguel Montiel <31743982+miguelmontiel30@users.noreply.github.com>
2025-05-04 12:38:19 -0600

    [refactor] Extracción de tipos a un nuevo módulo en el componente `DataTable`. - Se movieron las definiciones de tipos `SortDirection`, `Column` y `DataTableProps` a un nuevo archivo `types.ts` dentro de `module-utils`, mejorando la organización del código y facilitando su reutilización en otros componentes.

--- src/components/core/table/DataTable.tsx
+++ src/components/core/table/DataTable.tsx
@@ -8,37 +8,8 @@ import { Table, TableHeader, TableBody, TableRow, TableCell } from './index';
 import IconFA from '@/components/ui/IconFA';
 import Input from '@/components/form/input/InputField';
 
-export type SortDirection = 'asc' | 'desc';
-
-export interface Column<T> {
-    key: keyof T | string;
-    header: string;
-    sortable?: boolean;
-    render?: (item: T) => ReactNode;
-    width?: string;
-    className?: string;
-    searchable?: boolean; // Indica si esta columna se usará para búsqueda
-}
-
-export interface DataTableProps<T> {
-    readonly data: T[];
-    readonly columns: Column<T>[];
-    readonly keyExtractor: (item: T) => string | number;
-    readonly searchable?: boolean;
-    readonly searchPlaceholder?: string;
-    readonly defaultSortField?: string;
-    readonly defaultSortDirection?: SortDirection;
-    readonly isLoading?: boolean;
-    readonly noDataMessage?: string;
-    readonly searchNoResultsMessage?: string;
-    readonly className?: string;
-    readonly itemsPerPage?: number;
-    readonly maxHeight?: string;
-    readonly emptyStateComponent?: ReactNode;
-    readonly loadingComponent?: ReactNode;
-    readonly onSearch?: (searchTerm: string) => void;
-    readonly onSort?: (field: string, direction: SortDirection) => void;
-}
+// Types
+import { DataTableProps, SortDirection, } from './module-utils/types';
 
 export default function DataTable<T>({
     data,
