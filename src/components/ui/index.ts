022e4251648b3503e6b2b2cce76e8603a3560a19
Miguel Montiel <31743982+miguelmontiel30@users.noreply.github.com>
2025-05-27 23:58:40 -0600

    [feat] Añadir componente `NotificationBadge` y actualizar el índice de componentes. - Se creó el componente `NotificationBadge` para mostrar notificaciones con diferentes tipos y conteos. - Se actualizó el archivo de índice para incluir el nuevo componente `NotificationBadge` y mejorar la organización de las exportaciones.

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,4 +1,8 @@
+import IconFA from './IconFA';
+import NotificationBadge from './NotificationBadge';
+
 export * from './modal';
 export {default as IconFA} from './IconFA';
 export {default as Collapse} from './Collapse';
 export type {IconFAProps, IconStyle, IconSize} from './IconFA';
+export {default as NotificationBadge} from './NotificationBadge';
